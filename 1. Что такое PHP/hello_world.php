a92855eb24244766e3f728b34f96ebffbcf7e059
docrot18 <80437818+docrot18@users.noreply.github.com>
2022-09-06 17:19:04 +0900

    Hello_World

---
+++ 1. Что такое PHP/hello_world.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello_world</title>
+    </head>
+    <body>
+
+        <?php
+        echo "Привет, я - скрипт PHP!";
+        ?>
+
+    </body>
+</html>
